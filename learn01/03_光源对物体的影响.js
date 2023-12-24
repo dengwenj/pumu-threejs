461a32e22b109ea6f23f79ad58cfb11ef57fd005
朴睦 <927561237@qq.com>
2023-12-24 15:44:15 +0800

    光源对物体的影响

---
+++ learn01/03_光源对物体的影响.js
@@ -0,0 +1,45 @@
+import * as THREE from 'three'
+
+// 创建场景
+const scene = new THREE.Scene()
+
+// 创建网格模型材质，有些网格模型材质没有光源
+const material = new THREE.MeshLambertMaterial({
+  color: 0x00ff00,
+  transparent: true,
+  opacity: 0.5
+})
+
+// 光源
+const light = new THREE.PointLight(0xffffff, 1.0) // 点光源的颜色，第二个参数是光照强度
+light.decay = 0.0 //设置光源不随距离衰减
+light.position.set(300, 500, 200)
+scene.add(light)
+
+// 创建物体
+const box = new THREE.BoxGeometry(100, 100, 100)
+
+// 辅助线
+const axes = new THREE.AxesHelper(150)
+scene.add(axes)
+
+// 创建网格模型
+const mesh = new THREE.Mesh(box, material) // 放到网格模型中
+// 网格模型的位置
+mesh.position.set(0, 10, 0)
+
+// 添加到场景中
+scene.add(mesh)
+
+// 创建一个透视投影相机
+const camera = new THREE.PerspectiveCamera(45, 800 / 500, 1, 3000)
+// 设置相机的位置
+camera.position.set(200, 200, 200)
+camera.lookAt(mesh.position) // 看到网格模型
+
+// 渲染器
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(800, 500)
+renderer.render(scene, camera)
+
+document.body.appendChild(renderer.domElement)
